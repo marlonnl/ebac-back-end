62ca51c4b24a18f75378a966f83277e3d352efb4
Marlonn Locatelli <marlonn@gmail.com>
2024-11-30 18:09:49 -0300

    add: exercícios do módulo 8 (tratamento de erros)

---
+++ Lab 8/ex4.py
@@ -0,0 +1,7 @@
+class Pessoa(object):
+	def __init__(self, nome: str, idade: int, documento: str):
+		self.nome = nome
+		self.idade = idade
+		self.documento = documento
+
+andre = Pessoa(nome="Andre", idade=30, documento="123")
